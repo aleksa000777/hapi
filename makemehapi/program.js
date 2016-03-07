7666b7170131c03ee66c8a2870d439d4f5e7e5dd
Aleksandra Matiyev <aleksa000777@mail.ru>
2016-03-07 16:45:39 -0500

    routes

--- makemehapi/program.js
+++ makemehapi/program.js
@@ -1,17 +1,28 @@
 var Hapi = require('hapi');
+var Inert = require('inert');
+var Path = require('path');
 var server = new Hapi.Server();
 
 server.connection({
   host: 'localhost',
   port: Number(process.argv[2] || 8080)
 });
+server.register(Inert, function(){
+  server.route({path: '/{name*}', method: 'GET', handler: {
+   file: Path.join(__dirname, 'index.html')
+  }
+});
 
-server.route({path: '/{name*}', method: 'GET', handler: handler});
+});
 
-function handler(request, reply) {
-  reply('Hello '+ encodeURIComponent(request.params.name));
-}
 
+// function handler(request, reply) {
+//   reply('Hello '+ encodeURIComponent(request.params.name));
+// }
+
+// server.register(Inert, function(err){
+//   if(err) throw err;
+// })
 //start server
 server.start(function(){
   console.log('Server running at:', server.info.uri);
